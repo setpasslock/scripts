82ede7bdf52b5621c0a7571292b8f4bfc18c3e14
Yusuf D <92121578+setpasslock@users.noreply.github.com>
2024-02-07 02:25:52 +0300

    Create find_email.py

---
+++ find_email.py
@@ -0,0 +1,23 @@
+import re
+import argparse
+
+arg_parser = argparse.ArgumentParser(description="Email finder")
+arg_parser.add_argument("-f", "--file",required=True, help="file name")
+
+args = vars(arg_parser.parse_args())
+file_name = args["file"]
+
+def find_emails(text):
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    emails = re.findall(email_pattern, text)
+    return emails
+
+def main():
+    with open(file_name, 'r') as file:
+        text = file.read()
+    emails = find_emails(text)
+    if emails:
+        for email in emails:
+            print(email)
+    else:
+        pass
