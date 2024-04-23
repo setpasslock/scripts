f5ae246e6154fd32cef9c9c6e924ec9ec2375f24
Yusuf D <92121578+setpasslock@users.noreply.github.com>
2024-04-23 19:20:33 +0300

    Create findpatt.go

---
+++ golang/findpatt.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+const IPRegex = `(\d{1,3}\.){3}\d{1,3}`
+
+func printIPPatterns(text string) {
+	re := regexp.MustCompile(IPRegex)
+	ipPatterns := re.FindAllString(text, -1)
+
+	for _, ip := range ipPatterns {
+		fmt.Println(ip)
+	}
+}
+
+func main() {
+	fileFlag := flag.String("f", "", "File path")
+	dirFlag := flag.String("d", "", "Directory path")
+	ipFlag := flag.Bool("ip", false, "Display IP address")
+
+	flag.Parse()
+
+	if *fileFlag != "" && *ipFlag {
+		filePath := *fileFlag
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+
+		printIPPatterns(string(content))
+	}
+
+	if *dirFlag != "" && *ipFlag {
+		dirPath := *dirFlag
+
+		files, err := os.ReadDir(dirPath)
+		if err != nil {
+			fmt.Println("Error reading directory:", err)
+			return
+		}
+
+		for _, file := range files {
+			if !file.IsDir() {
+				filePath := filepath.Join(dirPath, file.Name())
+				content, err := os.ReadFile(filePath)
+				if err != nil {
+					fmt.Printf("Error reading file %s: %v\n", file.Name(), err)
+					continue
+				}
+
+				fmt.Printf("IP patterns in file %s:\n", file.Name())
+				printIPPatterns(string(content))
+				fmt.Println()
+			}
+		}
+	}
+}
