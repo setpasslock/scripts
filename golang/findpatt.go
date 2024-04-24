283f5966ff7b8c1e5d7910aefaccc42ec84111c7
Yusuf D <92121578+setpasslock@users.noreply.github.com>
2024-04-24 18:46:08 +0300

    Update findpatt.go
    email regex added, ip regex changed

--- golang/findpatt.go
+++ golang/findpatt.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gabriel-vasile/mimetype"
 )
 
-const IPRegex = `(\d{1,3}\.){3}\d{1,3}`
+const IPRegex = `\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`
 
 // unstable
 const domainRegex = `^(https?:\/\/)?([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(:\d+)?(\/.*)?`
